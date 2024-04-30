95d499ae8a42bc35318f93634a4d07e541d21e02
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-04-30 13:05:42 +0100

    Hero background done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,44 @@
-import Image from "next/image";
-import Header from "./components/Header";
+"use client";
+
+import { useState, useEffect } from "react";
 import Layout from "./layout";
 
 export default function Home() {
+  const [backgroundImage, setBackgroundImage] = useState("Coding.jpeg");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setBackgroundImage((prevImage) =>
+        prevImage === "Coding.jpeg" ? "Countryside.jpeg" : "Coding.jpeg"
+      );
+    }, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <>
-      <Layout></Layout>
-    </>
+    <div className="min-h-screen relative overflow-hidden">
+      <div
+        className={`bg-cover bg-center absolute inset-0 transition-transform ease-in-out ${
+          backgroundImage === "Coding.jpeg" ? "slide-in" : "slide-out"
+        }`}
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/Coding.jpeg)`,
+          transitionDuration: "3s", // Adjust the transition duration here
+        }}
+      ></div>
+      <div
+        className={`bg-cover bg-center absolute inset-0 transition-transform ease-in-out ${
+          backgroundImage === "Countryside.jpeg" ? "slide-in" : "slide-out"
+        }`}
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/Countryside.jpeg)`,
+          transitionDuration: "3s", // Adjust the transition duration here
+        }}
+      ></div>
+      <Layout>{/* Rest of your content */}</Layout>
+    </div>
   );
 }
