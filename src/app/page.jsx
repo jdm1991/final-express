e3229b9c0346b7baca0b6d2be7fa41623d8ed88c
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-04-30 12:04:42 +0100

    Layout structure fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
-import Header from './components/Header';
-import Footer from './components/Footer'
-
+import Header from "./components/Header";
+import Layout from "./layout";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Footer />
+      <Layout></Layout>
     </>
   );
 }
