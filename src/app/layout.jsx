de1c8f80d63d4c4777dd8cfc0f9d92b82dafd6cd
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-05-07 12:52:08 +0100

    File restructure

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import "./styles/globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
 
 export default function RootLayout({ children }) {
   return (
