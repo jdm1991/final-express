03dd55ab5dd280a9d6a2c67e1e5d841c970b43c1
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-05-07 12:49:13 +0100

    Feeble attempt

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import "./styles/globals.css";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
