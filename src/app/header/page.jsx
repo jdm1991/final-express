e5223c485808b7d80e1617fbab019b3a19fff784
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-05-15 15:27:39 +0100

    Login button added to header

--- src/app/header/page.jsx
+++ src/app/header/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
 import Link from "next/link";
@@ -72,6 +72,24 @@ export default function Header() {
               >
                 Contact
               </Link>
+              <Link
+                href="/login"
+                className="text-white hover:text-[#ffcf40] px-3 py-2 rounded-md text-med font-medium"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 inline-block mr-1"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 0C4.486 0 0 4.486 0 10s4.486 10 10 10 10-4.486 10-10S15.514 0 10 0zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8zm1-7.414V5.5c0-.283-.217-.5-.5-.5h-1c-.283 0-.5.217-.5.5v4.586l-2.293-2.293c-.195-.195-.512-.195-.707 0l-.707.707c-.195.195-.195.512 0 .707L9.586 12l-2.293 2.293c-.195.195-.195.512 0 .707l.707.707c.195.195.512.195.707 0L11 13.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Login
+              </Link>
             </div>
           </div>
           <div className="flex lg:hidden">
@@ -136,10 +154,27 @@ export default function Header() {
             >
               Contact
             </Link>
+            <Link
+              href="/login"
+              className="text-white hover:text-[#ffcf40] px-3 py-2 rounded-md text-med font-medium"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 inline-block mr-1"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 0C4.486 0 0 4.486 0 10s4.486 10 10 10 10-4.486 10-10S15.514 0 10 0zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8zm1-7.414V5.5c0-.283-.217-.5-.5-.5h-1c-.283 0-.5.217-.5.5v4.586l-2.293-2.293c-.195-.195-.512-.195-.707 0l-.707.707c-.195.195-.195.512 0 .707L9.586 12l-2.293 2.293c-.195.195-.195.512 0 .707l.707.707c.195.195.512.195.707 0L11 13.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Login
+            </Link>
           </div>
         </div>
       )}
     </nav>
   );
-};
-
+}
