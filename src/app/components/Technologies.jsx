7c01d5ff6d0edc0c5d820fabc904375f6f2a9ff4
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-05-16 10:14:05 +0100

    Tweaking tech section

--- src/app/components/Technologies.jsx
+++ src/app/components/Technologies.jsx
@@ -5,27 +5,27 @@ export default function Technologies() {
   const technologies = [
     {
       name: "HTML",
-      logo: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png",
+      logo: "https://www.svgrepo.com/show/197982/html.svg",
     },
     {
       name: "CSS",
-      logo: "https://seeklogo.com/images/C/css-3-logo-023C1A7171-seeklogo.com.png",
+      logo: "https://www.svgrepo.com/show/353623/css-3.svg",
     },
     {
       name: "JavaScript",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/768px-JavaScript-logo.png",
+      logo: "https://www.svgrepo.com/show/373703/js.svg",
     },
     {
       name: "React",
-      logo: "https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png",
+      logo: "https://www.svgrepo.com/show/354259/react.svg",
     },
     {
       name: "Next.js",
-      logo: "https://www.svgrepo.com/show/354113/nextjs-icon.svg",
+      logo: "https://www.svgrepo.com/show/368858/nextjs.svg",
     },
     {
       name: "VS Code",
-      logo: "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png",
+      logo: "https://www.svgrepo.com/show/374171/vscode.svg",
     },
     {
       name: "TailwindCSS",
@@ -33,7 +33,7 @@ export default function Technologies() {
     },
     {
       name: "Bootstrap",
-      logo: "https://icons.veryicon.com/png/o/application/skills-section/bootstrap-2.png",
+      logo: "https://www.svgrepo.com/show/353498/bootstrap.svg",
     },
     {
       name: "ViteJS",
@@ -45,11 +45,11 @@ export default function Technologies() {
     },
     {
       name: "PostgreSQL",
-      logo: "https://p7.hiclipart.com/preview/396/90/545/postgresql-database-logo-computer-icons-replication-software-developer.jpg",
+      logo: "https://www.svgrepo.com/show/303301/postgresql-logo.svg",
     },
     {
       name: "Git",
-      logo: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      logo: "https://www.svgrepo.com/show/303548/git-icon-logo.svg",
     },
     {
       name: "Prisma",
@@ -61,7 +61,7 @@ export default function Technologies() {
     },
     {
       name: "Jasmine",
-      logo: "https://www.pngfind.com/pngs/m/68-684650_test-frameworks-jasmine-js-logo-png-transparent-png.png",
+      logo: "https://www.svgrepo.com/show/373695/jasmine.svg",
     },
   ];
   const [hoveredIndex, setHoveredIndex] = useState(null);
@@ -82,14 +82,14 @@ export default function Technologies() {
               style={{
                 transform: `scale(${
                   hoveredIndex === index
-                    ? 1.5
+                    ? 1.8
                     : hoveredIndex === index - 1 || hoveredIndex === index + 1
-                    ? 1.25
+                    ? 1.15
                     : 1
                 })`,
               }}
             >
-              <div className="w-16 h-16 relative">
+              <div className="w-14 h-14 relative">
                 <Image
                   src={tech.logo}
                   alt={tech.name}
