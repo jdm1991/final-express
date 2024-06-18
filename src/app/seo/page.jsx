de001194eee61590dcf144f6fe00b85986ebfd6e
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-06-18 11:41:33 +0100

    Title fixed

--- src/app/seo/page.jsx
+++ src/app/seo/page.jsx
@@ -11,11 +11,11 @@ export default function SEO() {
     <div className="bg-white text-black">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 pt-24">
         <div className="py-8 sm:py-12">
-          <h1 className="text-4xl sm:text-6xl font-bold mb-8">
+          <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-8 leading-tight">
             SEO (Search Engine Optimization)
           </h1>
           <div className="mb-8">
-            <h2 className="text-3xl sm:text-4xl font-bold mb-4">
+            <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4">
               What is SEO and do I need it?
             </h2>
             <p className={`text-lg mb-4`}>
