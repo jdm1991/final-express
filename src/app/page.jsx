fee0bccb1cc6e7421ee84f71814768d3d91f407a
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-06-27 15:23:50 +0100

    Latest fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,3 @@
-"use client";
-
-import Head from "next/head";
 import FadeInSection from "./components/FadeInSection";
 import HeroSection from "./components/HeroSection.jsx";
 import BuildaWebsite from "./components/BuildaWebsite.jsx";
@@ -11,50 +8,40 @@ import Technologies from "./components/Technologies.jsx";
 import GetInTouch from "./components/GetInTouch.jsx";
 import SmallPortfolio from "./components/SmallPortfolio.jsx";
 
+export const metadata = {
+  title: "Express Web Design | Professional Web Design & Development Services",
+  description:
+    "Express Web offers expert web design, development, and SEO services. Create stunning, functional websites tailored for your business needs.",
+};
+
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>
-          Express Web Design | Professional Web Design & Development Services
-        </title>
-        <meta
-          name="description"
-          content="Express Web offers expert web design, development, and SEO services. Create stunning, functional websites tailored for your business needs."
-        />
-        <meta
-          name="keywords"
-          content="web design, web development, SEO, responsive design, professional websites, Norfolk"
-        />
-      </Head>
-      <div className="">
-        {" "}
-        {/* Adjusted spacing between sections */}
-        <FadeInSection>
-          <HeroSection />
-        </FadeInSection>
-        <FadeInSection>
-          <BuildaWebsite />
-        </FadeInSection>
-        <FadeInSection>
-          <Process />
-        </FadeInSection>
-        <FadeInSection>
-          <Services />
-        </FadeInSection>
-        <FadeInSection>
-          <SmallPortfolio />
-        </FadeInSection>
-        <FadeInSection>
-          <Technologies />
-        </FadeInSection>
-        <FadeInSection>
-          <BlogPostTiles />
-        </FadeInSection>
-        <FadeInSection>
-          <GetInTouch />
-        </FadeInSection>
-      </div>
-    </>
+    <div className="">
+      {/* Adjusted spacing between sections */}
+      <FadeInSection>
+        <HeroSection />
+      </FadeInSection>
+      <FadeInSection>
+        <BuildaWebsite />
+      </FadeInSection>
+      <FadeInSection>
+        <Process />
+      </FadeInSection>
+      <FadeInSection>
+        <Services />
+      </FadeInSection>
+      <FadeInSection>
+        <SmallPortfolio />
+      </FadeInSection>
+      <FadeInSection>
+        <Technologies />
+      </FadeInSection>
+      <FadeInSection>
+        <BlogPostTiles />
+      </FadeInSection>
+      <FadeInSection>
+        <GetInTouch />
+      </FadeInSection>
+    </div>
   );
 }
