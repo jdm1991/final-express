73b73f3440620ce382aa9bf312d6c8d40d7238ed
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-07-01 13:10:32 +0100

    Frequently asked questions fixed

--- src/app/faqs/page.jsx
+++ src/app/faqs/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import WebsiteDesignFaqs from "../components/WebsiteDesignFaqs";
 import HostingFaqs from "../components/HostingFaqs";
@@ -10,15 +11,15 @@ export default function Faqs() {
     <FadeInSection>
       <div className="bg-white">
         <section
-          className="bg-gray-100 pt-24 pb-8 sm:pb-12 bg-cover bg-center relative"
+          className="bg-gray-100 pt-20 pb-8 sm:pt-24 sm:pb-12 bg-cover bg-center relative"
           style={{ backgroundImage: "url('/FAQ.jpeg')" }}
         >
           <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10 pt-4 sm:pt-8">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 text-white">
+          <div className="max-w-7xl mx-auto px-2 sm:px-4 md:px-6 lg:px-8 relative z-10 pt-4 sm:pt-8">
+            <h1 className="text-2xl xs:text-3xl sm:text-4xl md:text-5xl font-bold mb-4 text-white leading-tight tracking-tight">
               Frequently Asked Questions
             </h1>
-            <p className="text-lg mb-6 text-white">
+            <p className="text-sm xs:text-base sm:text-lg mb-6 text-white">
               Find answers to common questions about our services, processes,
               and more. If you can&apos;t find what you&apos;re looking for,
               don&apos;t hesitate to get in touch with us.
